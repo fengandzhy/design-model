56e226c2c1ef16fc8b80b05fd622ca0a2da03e46
fengandzhy <fengandzhy@gmail.com>
2020-03-08 22:48:21 +1300

    my thread test

---
+++ design-models/design-models-singleton/src/main/java/org/zhouhy/design/model/sigleton/md01/MyThread.java
@@ -0,0 +1,20 @@
+package org.zhouhy.design.model.sigleton.md01;
+
+public class MyThread extends Thread {
+
+	@Override
+	public void run() {
+		System.out.println(MySingleton.getInstance().hashCode());
+	}
+	
+	public static void main(String args[]) {
+		MyThread[] mts = new MyThread[10];
+		for(int i=0;i<mts.length;i++) {
+			mts[i] = new MyThread();
+		}		
+		for(int i=0;i<mts.length;i++) {
+			mts[i].start();
+		}		
+	}
+	
+}
