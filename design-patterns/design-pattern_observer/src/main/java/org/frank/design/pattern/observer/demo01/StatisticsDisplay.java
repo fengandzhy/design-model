56498897965c152f5b83dbba1d4e3442c0f387c1
fengandzhy <fengandzhy@gmail.com>
2023-09-25 20:30:21 +1300

    design-pattern_observer

---
+++ design-patterns/design-pattern_observer/src/main/java/org/frank/design/pattern/observer/demo01/StatisticsDisplay.java
@@ -0,0 +1,8 @@
+package org.frank.design.pattern.observer.demo01;
+
+public class StatisticsDisplay implements Observer{
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        
+    }
+}
