0b50bd60bcdb30381abc99d55cf4765198c8b094
fengandzhy <fengandzhy@gmail.com>
2023-09-25 19:44:36 +1300

    design-pattern_observer

---
+++ design-patterns/design-pattern_observer/src/main/java/org/frank/design/pattern/observer/demo01/Observer.java
@@ -0,0 +1,5 @@
+package org.frank.design.pattern.observer.demo01;
+
+public interface Observer {
+    void update(float temperature, float humidity, float pressure);
+}
--- design-patterns/design-pattern_observer/src/main/java/org/frank/design/pattern/observer/demo01/Subject.java
+++ design-patterns/design-pattern_observer/src/main/java/org/frank/design/pattern/observer/demo01/Subject.java
@@ -1,5 +1,7 @@
 package org.frank.design.pattern.observer.demo01;
 
 public interface Subject {
-    
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
 }
