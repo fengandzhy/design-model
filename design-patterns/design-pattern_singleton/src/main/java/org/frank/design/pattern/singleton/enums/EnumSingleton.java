e8e4010fa35b17459d2f6375349f0733694d5868
fengandzhy <fengandzhy@gmail.com>
2024-03-23 12:31:22 +1300

    singleton pattern

---
+++ design-patterns/design-pattern_singleton/src/main/java/org/frank/design/pattern/singleton/enums/EnumSingleton.java
@@ -0,0 +1,8 @@
+package org.frank.design.pattern.singleton.enums;
+
+public enum EnumSingleton {
+    INSTANCE;    
+    public void doSomething(){
+        System.out.println("Doing something...");
+    }    
+}
