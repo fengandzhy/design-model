746d2f9738215aa7fa9df300381da5a2bd6d121f
fengandzhy <fengandzhy@gmail.com>
2024-03-27 10:15:19 +1300

    mediator pattern

--- design-patterns/design-pattern_command/src/main/java/org/frank/design/pattern/command/demo01/CommandDemo01Test.java
+++ design-patterns/design-pattern_command/src/main/java/org/frank/design/pattern/command/demo01/CommandDemo01Test.java
@@ -5,6 +5,10 @@ import org.frank.design.pattern.command.demo01.command.LightOnCommand;
 import org.frank.design.pattern.command.demo01.invoker.RemoteControl;
 import org.frank.design.pattern.command.demo01.receiver.Light;
 
+/**
+ * 它将一个请求封装为一个对象，从而让你使用不同的请求、队列或日志请求来参数化其他对象。
+ * 
+ * */
 public class CommandDemo01Test {
     public static void main(String[] args) {
         Light light = new Light();
