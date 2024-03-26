4e586730eba7c3694c411535a1eef66e76635227
fengandzhy <fengandzhy@gmail.com>
2024-03-27 10:17:07 +1300

    mediator pattern

--- design-patterns/design-pattern_mediator/src/main/java/org/frank/design/pattern/mediator/demo01/MediatorDemo01Test.java
+++ design-patterns/design-pattern_mediator/src/main/java/org/frank/design/pattern/mediator/demo01/MediatorDemo01Test.java
@@ -5,6 +5,10 @@ import org.frank.design.pattern.mediator.demo01.colleague.Boeing737;
 import org.frank.design.pattern.mediator.demo01.mediator.AirTrafficControl;
 import org.frank.design.pattern.mediator.demo01.mediator.Mediator;
 
+/**
+ * 中介者模式（Mediator Pattern）是一种行为设计模式，旨在减少对象之间的直接通信，并通过中介者对象来协调对象之间的交互。
+ * 在中介者模式中，对象之间不再直接相互引用，而是通过一个中介者对象来进行通信和协调。
+ * */
 public class MediatorDemo01Test {
     public static void main(String[] args) {
         Mediator mediator = new AirTrafficControl();
