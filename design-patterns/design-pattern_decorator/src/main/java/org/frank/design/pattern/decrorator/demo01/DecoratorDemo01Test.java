cea02c5acee8d9711dd1bb8cb3fd1b90ecf3cfa7
fengandzhy <fengandzhy@gmail.com>
2024-03-27 10:52:22 +1300

    mediator pattern

--- design-patterns/design-pattern_decorator/src/main/java/org/frank/design/pattern/decrorator/demo01/DecoratorDemo01Test.java
+++ design-patterns/design-pattern_decorator/src/main/java/org/frank/design/pattern/decrorator/demo01/DecoratorDemo01Test.java
@@ -5,6 +5,9 @@ import org.frank.design.pattern.decrorator.demo01.component.impl.BasicCoffee;
 import org.frank.design.pattern.decrorator.demo01.decorator.impl.MilkDecorator;
 import org.frank.design.pattern.decrorator.demo01.decorator.impl.SugarDecorator;
 
+/**
+ * 主要作用是在不改变原始对象的基础上，动态地给对象添加额外的功能或责任。它允许向对象添加新的功能，同时又不改变其结构
+ * */
 public class DecoratorDemo01Test {
 
     public static void main(String[] args) {
